6c6fb583827c6effa12c59120ddcdf030b128d39
Jeff Liu <boyuanliu@live.com>
2018-07-06 14:00:01 -0400

    Random Blocks of colors functionalitiy

--- color_game.js
+++ color_game.js
@@ -1,14 +1,7 @@
-var colors = [
-	"rgb(255, 0, 0)",
-	"rgb(255, 255, 0)",
-	"rgb(0, 255, 0)",
-	"rgb(0, 255, 255)",
-	"rgb(0, 0, 255)",
-	"rgb(255, 0, 255)"
-	]
+var colors = generateRandomColors(6);
 
 var squares = document.querySelectorAll(".square");
-var pickedColor = colors[3];
+var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message")
 
@@ -25,6 +18,7 @@ for (var i = 0; i < squares.length; i++) {
 		//compare color to picked color
 		if(clickedColor === pickedColor){
 			messageDisplay.textContent = "Correct"
+			changeColors(clickedColor);
 		}
 		else{
 			this.style.backgroundColor = "#232323";
@@ -34,3 +28,44 @@ for (var i = 0; i < squares.length; i++) {
 	})
 }
 
+
+function changeColors(color){
+//loop through all squares and change each color to match given color
+	for (var i = 0; i < squares.length; i++) {
+		squares[i].style.backgroundColor = color;
+
+	}
+//change h1 to color
+
+}
+
+function pickColor(){
+	var random = Math.floor(Math.random() * colors.length);
+	return colors[random];
+}
+
+function generateRandomColors(num){
+	//make array with num length 
+	var arr = [];
+
+	for (var i = 0; i < num; i++) {
+		//get rand color and push into array
+		arr.push(randomColor());
+	}
+
+
+	return arr;
+}
+
+function randomColor(){
+	//pick red from 0-255
+	//pick green from 0-255
+	//pick blue from 0-255
+
+	var r = Math.floor(Math.random()*256)
+	var g = Math.floor(Math.random()*256)
+	var b = Math.floor(Math.random()*256)
+
+	//"rgb(r, g, b)"
+	return "rgb(" + r + ", " + g + ", " + b + ")";
+}
